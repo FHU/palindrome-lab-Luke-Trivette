a8bdc1ac01d48007395bb288c6b52041bbb5ff76
Luke-Trivette <157540127+Luke-Trivette@users.noreply.github.com>
2024-02-10 13:30:38 -0600

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    cleaned_text = "".join(word.split()).lower()
+    return cleaned_text == cleaned_text[::-1]
 
 if __name__ == '__main__': 
     #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    user_input = input("Enter a text: ")
+    result = palindrome(user_input)
+    if result:
+        print("The entered text is a palindrome.")
+    else:
+        print("The entered text is not a palindrome.")
